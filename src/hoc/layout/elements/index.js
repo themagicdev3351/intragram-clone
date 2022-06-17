a0ffb9759a90913c9471d0972f335d1c46b62e0f
themagicdev3351 <themagicdev3351@gmail.com>
2022-06-17 22:42:03 +0530

    MY first commit

---
+++ src/hoc/layout/elements/index.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+import { Form } from 'formik';
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 1250px;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto; 
+  height: 100%;
+`;
+
+export const FormWrapper = styled.div`
+  width: 100%;
+  max-width: 60rem;
+  margin: 0 auto;
+  border-radius: 0.7rem;
+  padding: 8rem 7rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--color-mainDark);
+  box-shadow: 0rem 0.5rem 3.5rem var(--shadow);
+`;
+
+export const StyledForm = styled(Form)`
+  display: flex;
+  position: relative;
+  align-items: center;
+  width: 100%;
+  flex-direction: column;
+`;
---
+++ src/utils/theme.js
@@ -0,0 +1,23 @@
+const theme = {
+  colors: {
+    main: '#003459',
+    mainDark: '#21283e',
+    mainLight: '#2c3247',
+    mainLighter: '#2f82b8',
+    textColor: '#333',
+    whiteColor: '#fff',
+    errorRed: '#ff5757',
+    shadow: 'rgba(0,0,0,.2)',
+    mainBhautik:'red',
+  },
+
+  mediaQueries: {
+    smallest: `only screen and (max-width: 567px)`,
+    small: `only screen and (max-width: 767px)`,
+    medium: `only screen and (max-width: 991px)`,
+    large: `only screen and (max-width: 1199px)`,
+    largest: `only screen and (max-width: 1400px)`,
+  },
+};
+
+export default theme;
